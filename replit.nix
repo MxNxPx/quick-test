22ffb4b8e5eb9b83cf8cbeb34c1e9b7fece9c7ae
MxNxPx <MxNxPx@gmail.com>
2024-04-19 08:43:38 -0400

    fix: more tweaks to replit

--- replit.nix
+++ replit.nix
@@ -2,13 +2,9 @@
     deps = [
       pkgs.cowsay
       pkgs.curl
-      pkgs.git
       pkgs.jq
-      pkgs.screen
       pkgs.terraform
-      pkgs.tmux
       pkgs.unzip
-      pkgs.wget
       pkgs.bashInteractive
       pkgs.nodePackages.bash-language-server
       pkgs.man 
